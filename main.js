4992c45cecaf1dd8672f98c485b033a588addfeb
Yasomalako <Yasomalako@gmail.com>
2022-06-23 11:28:57 +0300

    promis

---
+++ main.js
@@ -0,0 +1,20 @@
+//1
+// function returnPromes() {
+//     return new Promise((resolve, reject) => {
+//         const timer = new Date().getHours()
+//         if (timer > 11) {
+//            resolve("goodluke" + "goodmorning")
+//         }
+//         reject("Eror" + "goodEvning")
+//     });
+// }
+// returnPromes()
+//     .then((data) => { document.write(data) })
+//     .catch((err) => { document.write(err) })
+
+//3
+function printPromes(num1,num2){
+return new Promise((resolve,reject)=>{
+    
+})
+}
