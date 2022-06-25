d682e4d104485657ad19f19c9b7b7708db588109
Yasomalako <Yasomalako@gmail.com>
2022-06-25 20:04:15 +0300

    promis

--- main.js
+++ main.js
@@ -25,22 +25,46 @@
 // .then((data)=> {document.write(data)})
 // .catch((err)=> {document.write(err)})
 //5
+
 // function checkArray(myArray,num){
 // return new Promise((resolve,reject)=>{
+//   setTimeout(()=>{ 
 //     if(myArray.includes(num)){
-//         resolve("goodluck")
-//     }
-//     reject("no")
-// })
+//     resolve("goodluck")
+// }
+// reject("no")
+// })},1000)
 // }   
 // checkArray([5,2,7,9],5)
 // .then((data)=>{document.write(data)})
 // .catch((err)=>{document.write(err)})
-//6
-
-
+//8
+function checkArray(myArray, num) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (myArray.includes(num)) {
+        resolve("goodluck")
+      }
+      reject("no")
+    })
+  }, 1000)
+}
+async function printAsynkfive() {
+  try {
+    let result = await checkArray([8, 2, 7, 9], 5);
+    console.log(`have ${result}`);
+  }
+  catch (error) {
+    console.log(error )
+  }
+  finally {
+    h1_catch.innerHTML = "";
+    arraycheck.disabled = false;
+  }
+}
+printAsynkfive()
 //fetch
-//1 
+//1
 // async function fechingUrl(){
 //     try{
 // await fetch('http://moviesmern.herokuapp.com/movies/all')
