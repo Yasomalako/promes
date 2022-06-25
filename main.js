7f150aba93dddf0b4f30e7742cbe4b44a0166f8a
Yasomalako <Yasomalako@gmail.com>
2022-06-25 20:53:36 +0300

    nn

--- main.js
+++ main.js
@@ -39,38 +39,60 @@
 // .then((data)=>{document.write(data)})
 // .catch((err)=>{document.write(err)})
 //8
-function checkArray(myArray, num) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (myArray.includes(num)) {
-        resolve("goodluck")
-      }
-      reject("no")
-    })
-  }, 1000)
-}
-async function printAsynkfive() {
-  try {
-    let result = await checkArray([8, 2, 7, 9], 5);
-    console.log(`have ${result}`);
-  }
-  catch (error) {
-    console.log(error )
-  }
-  finally {
-    h1_catch.innerHTML = "";
-    arraycheck.disabled = false;
-  }
-}
-printAsynkfive()
+// function checkArray(myArray, num) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (myArray.includes(num)) {
+//         resolve("goodluck")
+//       }
+//       reject("no")
+//     })
+//   }, 1000)
+// }
+// async function printAsynkfive() {
+//   try {
+//     let result = await checkArray([8, 2, 7, 9], 5);
+//     console.log(`have ${result}`);
+//   }
+//   catch (error) {
+//     console.log(error )
+//   }
+//   finally {
+//     h1_catch.innerHTML = "";
+//     arraycheck.disabled = false;
+//   }
+// }
+// printAsynkfive()
 //fetch
-//1
+//1/2/3
 // async function fechingUrl(){
 //     try{
+//       h1_catch.innerHTML="<img style='width=20vw' src='friends-loading-gif.gif'>"
+//       printer.disabled= true;
 // await fetch('http://moviesmern.herokuapp.com/movies/all')
 // .then(data =>console.log(data))
 //     }
 //     catch(eror){
 //         alert(eror)
 //     }
+//     finally{
+//       h1_catch.innerHTML='';
+//       printer.disabled=false;
+//     }
 // }
+//5
+async function fettchingHuro(){
+  try{
+    h1_catch.innerHTML="<img style='width=20vw' src='friends-loading-gif.gif'>"
+    printer_huro.disabled=true;
+    await fetch('https://cat-fact.herokuapp.com/facts')
+    .then(data => console.log(data))
+  }
+  catch(Eror){
+    console.log(Eror)
+  }
+  finally{
+    h1_catch.innerHTML="";
+    printer_huro.disabled=false;
+  }
+}
